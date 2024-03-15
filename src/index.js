ee61aeb3c2ef6ae6295ba6ac55ecfc6d426867b9
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2024-03-15 16:28:27 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style/style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const mainEl = document.querySelector('.wrapper');
+const root = ReactDOM.createRoot(mainEl);
+
+// root.render(
+//   <>
+    
+//   </>
+// );
